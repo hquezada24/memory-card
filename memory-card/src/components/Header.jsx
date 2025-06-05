48821090aa943bd1ac7bcd2072d41f111c80c5f4
Hugo <hquezada@leomail.tamuc.edu>
2025-06-05 17:09:39 -0500

    Add padding to footer. Modify h1 element in Header.jsx.

--- memory-card/src/components/Header.jsx
+++ memory-card/src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <>
       <header>
-        <h1>Memory game</h1>
+        <h1>El Chavo Memory Card</h1>
         <div className="score">
           <p>Score: </p>
           <p>Best: </p>
