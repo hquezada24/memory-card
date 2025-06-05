3b1a7f8d3fe403a5cf144189fc0af5ab5ad00edb
Hugo <hquezada@leomail.tamuc.edu>
2025-06-05 14:04:09 -0500

    Create header with title and score board. Modify the header css file.

--- memory-card/src/components/Header.jsx
+++ memory-card/src/components/Header.jsx
@@ -3,8 +3,13 @@ import "./Header.css";
 const Header = () => {
   return (
     <>
-      <header>Hello</header>
-      <button>hi</button>
+      <header>
+        <h1>Memory game</h1>
+        <div className="score">
+          <p>Score: </p>
+          <p>Best: </p>
+        </div>
+      </header>
     </>
   );
 };
