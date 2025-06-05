d80a50cfdafba164c06f57bc88d99c046027b9ed
Hugo <hquezada@leomail.tamuc.edu>
2025-06-05 15:12:40 -0500

    Install Giphy sdk. Import GiphyFetch into the Board.jsx file.

--- memory-card/src/components/Board.jsx
+++ memory-card/src/components/Board.jsx
@@ -1,21 +1,14 @@
 import "./Board.css";
 import Card from "./Card";
+import React, { useEffect, useState } from "react";
+import { GiphyFetch } from "@giphy/js-fetch-api";
+
+const gf = new GiphyFetch("8HHHmXteYm004PapntSOri2fQ1dZ5fiF");
 
 const Board = () => {
   return (
     <>
       <div className="board">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
         <Card />
       </div>
     </>
