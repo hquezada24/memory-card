9cf2d1298bee16ae77ad27bd689104e9ada468d1
Hugo <hquezada@leomail.tamuc.edu>
2025-06-05 14:11:13 -0500

    Create footer and footer style sheet.

--- memory-card/src/App.jsx
+++ memory-card/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
---
+++ memory-card/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>Made by Hugo Quezada</p>
+        <div className="social">
+          <a href="https://github.com/hquezada24" target="_blank">
+            GitHub
+          </a>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
