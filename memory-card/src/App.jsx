628f4d923d4b3dcf1e0284db3dffcde5da13fb14
Hugo <hquezada@leomail.tamuc.edu>
2025-06-05 14:54:19 -0500

    Create Board and Card components and their styling sheets.

--- memory-card/src/App.jsx
+++ memory-card/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Board from "./components/Board";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
+      <Board />
       <Footer />
     </>
   );
---
+++ memory-card/src/components/Board.jsx
@@ -0,0 +1,25 @@
+import "./Board.css";
+import Card from "./Card";
+
+const Board = () => {
+  return (
+    <>
+      <div className="board">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </>
+  );
+};
+
+export default Board;
