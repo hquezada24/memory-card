ab9b643dcfda4853e0ec903c2c98436b80a3ca90
Hugo <hquezada@leomail.tamuc.edu>
2025-06-06 11:22:56 -0500

    Create utils folder, utils.js file and a shuffling array function.

---
+++ memory-card/src/utils/utils.js
@@ -0,0 +1,12 @@
+const shuffle = (array) => {
+  const newArray = [...array]; // Create a copy to avoid mutating the original
+
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]]; // Swap elements
+  }
+
+  return newArray;
+};
+
+export { shuffle };
